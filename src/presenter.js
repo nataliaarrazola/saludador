c9e56d70c8bf119dc81c8bf3c10e7d6135847f72
Natalia <natalia.arrazola@ucb.edu.bo>
2026-08-24 22:34:27 -0400

    fix(saludador):agregar saludo en ingles y  correcion de algunos errores

--- src/presenter.js
+++ src/presenter.js
@@ -1,4 +1,4 @@
-import saludar from "./saludador";
+import saludar from "./saludador.js";
 
 const idiomaSelect = document.querySelector("#idioma-select");
 const nombreInput = document.querySelector("#nombre-input");
@@ -16,5 +16,5 @@ form.addEventListener("submit", (event) => {
   const edad = Number.parseInt(edadInput.value) || 0;
   const horaActual = new Date().getHours();
 
-  div.innerHTML = "<p>" + saludar(nombre, horaActual, genero, edad,idioma) + "</p>";
+  div.innerHTML = "<p>" + saludar(nombre, horaActual, genero, edad, idioma) + "</p>";
 });
--- src/saludador.js
+++ src/saludador.js
@@ -1,4 +1,4 @@
-function obtenerSaludoPorHora(hora) {
+function obtenerSaludoPorHoraES(hora) {
   if (hora >= 6 && hora < 12) {
     return "Buenos días";
   } else if (hora >= 12 && hora < 20) {
@@ -8,40 +8,70 @@ function obtenerSaludoPorHora(hora) {
   }
 }
 
+function obtenerSaludoPorHoraEN(hora) {
+  if (hora >= 6 && hora < 12) {
+    return "Good morning";
+  } else if (hora >= 12 && hora < 20) {
+    return "Good afternoon";
+  } else {
+    return "Good evening";
+  }
+}
+
 function saludar(nombre, hora, genero, edad, idioma = "es") {
   if (hora === undefined) {
     hora = new Date().getHours();
   }
 
-  if (idioma === "es") {
-    const saludoHora = obtenerSaludoPorHora(hora);
+  if (idioma === "en") {
+    const saludoHora = obtenerSaludoPorHoraEN(hora);
     let prefijo = "";
 
     if (edad > 30) {
       if (genero === "M") {
-        prefijo = " Sr. ";
+        prefijo = " Mr. ";
       } else if (genero === "F") {
-        prefijo = " Sra. ";
+        prefijo = " Mrs. ";
       } else if (nombre) {
         prefijo = " ";
       }
     } else {
-      if (genero === "M") {
-        prefijo = " estimado ";
-      } else if (genero === "F") {
-        prefijo = " estimada ";
+      if (genero === "M" || genero === "F") {
+        prefijo = " dear ";
       } else if (nombre) {
         prefijo = " ";
       }
     }
-
     if (nombre) {
       return saludoHora + prefijo + nombre;
     }
     return saludoHora;
   }
 
-  return "Hola";
+  const saludoHora = obtenerSaludoPorHoraES(hora);
+  let prefijo = "";
+
+  if (edad > 30) {
+    if (genero === "M") {
+      prefijo = " Sr. ";
+    } else if (genero === "F") {
+      prefijo = " Sra. ";
+    } else if (nombre) {
+      prefijo = " ";
+    }
+  } else {
+    if (genero === "M") {
+      prefijo = " estimado "; 
+    } else if (genero === "F") {
+      prefijo = " estimada ";
+    } else if (nombre) {
+      prefijo = " ";
+    }
+  }
+  if (nombre) {
+    return saludoHora + prefijo + nombre;
+  }
+  return saludoHora;
 }
 
 export default saludar;
